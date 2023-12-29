66970fe1a3ce6d60ca0bc05c9c7b2725cbd7f2de
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2023-12-30 03:21:53 +0530

    Creation and Printing done

--- Singly Linked List/SLL.java
+++ Singly Linked List/SLL.java
@@ -1,7 +1,37 @@
 public class SLL {
 
-    public static void main(String[] args) {
-        
+    private ListNode head;
+    private static class ListNode {
+        private int data;
+        private ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
     }
-    
+
+    public void display() {
+        ListNode currentListNode = head;
+        while (currentListNode != null) {
+            System.out.print(currentListNode.data+"->");
+            currentListNode=currentListNode.next;
+        }
+        System.out.print("NULL");
+    }
+
+    public static void main(String[] args) {
+        SLL sll = new SLL();
+        sll.head = new ListNode(10);
+        ListNode second = new ListNode(20);
+        ListNode third = new ListNode(30);
+
+        // Lets connect the data with each other
+        sll.head.next = second;
+        second.next = third;
+        third.next = null; // if not written then it will be treated as Null only
+
+        sll.display();
+    }
+
 }
