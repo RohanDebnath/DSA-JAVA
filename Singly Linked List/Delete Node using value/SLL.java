4dfb9bcb1633d5176b22aadb4f7ffad833986b2f
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2024-01-01 00:47:06 +0530

    Delete Node using value done

---
+++ Singly Linked List/Delete Node using value/SLL.java
@@ -0,0 +1,76 @@
+import java.util.Scanner;
+
+public class SLL {
+    private ListNode head;
+    private static class ListNode {
+        private ListNode next;
+        private int data;
+
+        public ListNode(int data)
+        {
+            this.data=data;
+            this.next=null;
+        }
+        
+    }
+    void addNode(int data)
+    {
+        ListNode newNode=new ListNode(data);
+        if(head==null)
+        {
+            head=newNode;
+        }else{
+            ListNode currentListNode=head;
+            while (currentListNode.next!=null) {
+                currentListNode=currentListNode.next;
+            }
+            currentListNode.next=newNode;
+        }
+    }
+    void display()
+    {
+        ListNode currentListNode=head;
+        while (currentListNode!=null) {
+            System.out.print(currentListNode.data+"->");
+            currentListNode=currentListNode.next;
+        }
+        System.out.print("NULL");
+    }
+    void deleteANodeUsingValue(int val)
+    {
+        ListNode currentListNode=head;
+        ListNode temp=null;
+        while (currentListNode!=null && currentListNode.data!=val) {
+            temp=currentListNode;
+            currentListNode=currentListNode.next;
+        }
+        if(currentListNode!=null)
+        {
+            temp.next=currentListNode.next;
+        }
+    }
+
+   public static void main(String[] args) {
+        SLL sll= new SLL();
+        boolean decision=true;
+        while (decision) {
+            System.out.println("Do you want to insert a Node? Y/N");
+            Scanner sc= new Scanner(System.in);
+            char choice=sc.nextLine().charAt(0);
+            if(choice=='y'||choice=='Y')
+            {
+                System.out.println("Enter Your data");
+                int data=sc.nextInt();
+                sll.addNode(data);
+
+            }else if(choice=='n'||choice=='N')
+            {
+                break;
+            }
+        }
+        sll.display();
+        System.out.println();
+        sll.deleteANodeUsingValue(20);
+        sll.display();
+    }    
+}
