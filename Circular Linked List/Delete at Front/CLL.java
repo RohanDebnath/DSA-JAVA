a53232d15cf0eb3ad5088201176ace63a269838e
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2024-01-02 20:57:15 +0530

    Delete at front done

---
+++ Circular Linked List/Delete at Front/CLL.java
@@ -0,0 +1,81 @@
+import java.util.Scanner;
+
+public class CLL {
+    private ListNode last;
+    private static int length = 0;
+
+    private static class ListNode {
+        private ListNode next;
+        private int data;
+
+        public ListNode(int data) {
+            this.data = data;
+        }
+    }
+
+    boolean isEmpty() {
+        return length == 0;
+    }
+
+    void addNode(int val) {
+        ListNode newListNode = new ListNode(val);
+        if (last == null) {
+            last = newListNode;
+            last.next = last;
+        } else {
+            newListNode.next = last.next;
+            last.next = newListNode;
+            last = newListNode;
+        }
+        length++;
+    }
+
+    void display() {
+        ListNode currentListNode = last.next;
+        if (last == null) {
+            return;
+        } else {
+            while (currentListNode != last) {
+                System.out.print(currentListNode.data + "->");
+                currentListNode = currentListNode.next;
+            }
+            System.out.print(currentListNode.data + "->Null");
+        }
+    }
+
+    void deleteAtFront() {
+        ListNode dummyListNode = last.next;
+        if (isEmpty()) {
+            System.out.println("Nothing to delete");
+        } else {
+            last.next=dummyListNode.next;
+        }
+        dummyListNode.next=null;
+        length--;
+    }
+
+    public static void main(String[] args) {
+        CLL cll = new CLL();
+        boolean decision = true;
+        while (decision) {
+            System.out.println("Do you want to insert Data? Y/N");
+            Scanner sc = new Scanner(System.in);
+            char choice = sc.nextLine().charAt(0);
+            if (choice == 'y' || choice == 'Y') {
+                System.out.println("Please enter your value");
+                int val = sc.nextInt();
+                cll.addNode(val);
+            } else if (choice == 'n' || choice == 'N') {
+                break;
+            }
+
+        }
+        cll.display();
+        System.out.println();
+        System.out.println(CLL.length);
+        cll.deleteAtFront();
+        cll.display();
+        System.out.println();
+        System.out.println(CLL.length);
+    }
+}
