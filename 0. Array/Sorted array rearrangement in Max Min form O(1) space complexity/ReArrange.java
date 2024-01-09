ad157f5ac8c879a07d92aa9b7948f6cf95f56a75
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2024-01-09 19:38:00 +0530

    O(1) space Rearrange Max min done

---
+++ 0. Array/Sorted array rearrangement in Max Min form O(1) space complexity/ReArrange.java
@@ -0,0 +1,38 @@
+public class ReArrange {
+ 
+    void display(int arr[])
+    {
+        for (int i : arr) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
+    }
+    int[] reArrange_SortMaxMin(int arr[])
+    {
+        int minIn=0;
+        int maxIn=arr.length-1;
+        int max=arr[arr.length-1]+1;
+        for (int i = 0; i < arr.length; i++) {
+            if(i==0){
+                arr[i]=arr[i]+(arr[maxIn%10])*10;
+                maxIn--;
+            }else{
+                arr[i]=arr[i]+(arr[minIn]%10)*10;
+                minIn++;
+            }
+        }
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=arr[i]/max;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int arr[]=new int[]{2,3,5,6,8,9};
+        ReArrange reArrange=new ReArrange();
+        reArrange.display(arr);
+        arr=reArrange.reArrange_SortMaxMin(arr);
+        System.out.println("After Sorting");
+        reArrange.display(arr);
+    }
+}
