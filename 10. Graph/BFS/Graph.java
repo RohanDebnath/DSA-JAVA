a56b1eceb31161dddc06ffa0935302a9a43a024d
RohanDebnath <86291016+RohanDebnath@users.noreply.github.com>
2024-01-10 02:56:17 +0530

    BFS done

---
+++ 10. Graph/BFS/Graph.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+public class Graph {
+    private LinkedList<Integer> adj[];
+    private int E;
+    private int V;
+
+    public Graph(int nodes) {
+        this.V = nodes;
+        this.E = 0;
+        this.adj = new LinkedList[nodes];
+        for (int i = 0; i < V; i++) {
+            adj[i] = new LinkedList<>();
+        }
+    }
+
+    public void addEdge(int u, int v) {
+        adj[u].add(v);
+        adj[v].add(u);
+        E++;
+    }
+
+    String printingGraph() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(V + " Vertices " + E + " Edges\n");
+        for (int v = 0; v < V; v++) {
+            sb.append(v + ": ");
+            for (int w : adj[v]) {
+                sb.append(w + " ");
+            }
+            sb.append("\n");
+        }
+        return sb.toString();
+    }
+
+    public void bfs(int s) {
+        boolean[] visited = new boolean[V];
+        Queue<Integer> q = new LinkedList<>();
+        visited[s] = true;
+        q.offer(s);
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            System.out.print(u + " ");
+            for (int v : adj[u]) {
+                if (!visited[v]) {
+                    visited[v] = true;
+                    q.offer(v);
+                }
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Graph g = new Graph(4);
+        g.addEdge(0, 1);
+        g.addEdge(1, 2);
+        g.addEdge(2, 3);
+        g.addEdge(3, 0);
+        System.out.println(g.printingGraph());
+        g.bfs(0);
+
+    }
+}
---
+++ Z_Beta/Test.java
@@ -0,0 +1,8 @@
+public class Test {
+    public static void main(String[] args) {
+        int n=10;
+        StringBuilder sb= new StringBuilder();
+        sb.append(n);
+        System.out.println(sb);
+    }
+}
